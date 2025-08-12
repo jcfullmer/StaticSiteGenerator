8706960ef25ee8a1aea8a3249f0fcb21b2452cf5
jcfullmer <clayefullmer@gmail.com>
2025-08-12 11:44:21 -0400

    adjusted code to be able to use github pages using the build.sh script

--- src/main.py
+++ src/main.py
@@ -2,20 +2,25 @@ from copystatic import cp_source_to_dest
 from page_generator import generate_page, generate_pages_recursively
 import os
 import shutil
+import sys
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_docs = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 def main():
-    print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    if len(sys.argv) > 1:
+        basepath = sys.argv [1]
+    else:
+        basepath = "/"
+    print("Deleting docs  directory...")
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
     print("Copying files to public directory")
-    cp_source_to_dest(dir_path_static, dir_path_public)
+    cp_source_to_dest(dir_path_static, dir_path_docs)
     print("Generating page-")
-    generate_pages_recursively(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursively(dir_path_content, template_path, dir_path_docs, basepath)
 
 
 
--- src/page_generator.py
+++ src/page_generator.py
@@ -12,7 +12,7 @@ def extract_title(markdown):
     return temp
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as f:
         mdfile = f.read()
@@ -23,6 +23,8 @@ def generate_page(from_path, template_path, dest_path):
     page_title = extract_title(mdfile)
     final_file = template_file.replace("{{ Title }}", page_title)
     final_file = final_file.replace("{{ Content }}", html)
+    final_file = final_file.replace('href="/', f'href="{basepath}')
+    final_file = final_file.replace('src="/', f'src="{basepath}')
     dest_dir_path = os.path.dirname(dest_path)
     
     if dest_dir_path != "":
@@ -33,7 +35,7 @@ def generate_page(from_path, template_path, dest_path):
 
 
 
-def generate_pages_recursively(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursively(dir_path_content, template_path, dest_dir_path, basepath):
         for item in os.listdir(dir_path_content):
             print(item)
             from_path = os.path.join(dir_path_content, item)
@@ -41,6 +43,6 @@ def generate_pages_recursively(dir_path_content, template_path, dest_dir_path):
             print(from_path, dest_path)
             if os.path.isfile(from_path):
                 print(f"Copying {item} to {dest_dir_path}")
-                generate_page(from_path, template_path, dest_path)
+                generate_page(from_path, template_path, dest_path, basepath)
             if os.path.isdir(from_path):
-                generate_pages_recursively(from_path, template_path, dest_path)
+                generate_pages_recursively(from_path, template_path, dest_path, basepath)
